db425f088d036f2b2bc9111cca3b682bab4eb8b2
jval <joshvaldez3320@gmail.com>
2019-02-25 20:14:13 -0700

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,7 +20,7 @@ var randomWords = [
 ];
 var chosenWord = "";
 var lettersGuessed = [];
-var totalGuesses = 10;
+var wrongGuesses = 10;
 var wins = 0;
 var underScores = [];
 var lettersInChosenWord = [];
@@ -29,50 +29,92 @@ var lettersGuessedText = " ";
 
 // Start Game document ready function
 function startGame() {
+    chosenWord = "";
+    lettersGuessed = [];
+    wrongGuesses = 10;
+    underScores = [];
+    lettersInChosenWord = [];
+    lettersGuessedText = " ";
     document.getElementById("lettersGuessed").innerHTML = lettersGuessedText + " ";
     document.getElementById("totalWins").innerHTML = wins;
+    document.getElementById("guessesLeft").innerHTML = wrongGuesses;
+
 
     // pick a random word
     chosenWord = randomWords[Math.floor(Math.random() * randomWords.length)]
-    console.log(chosenWord)
     lettersInChosenWord = chosenWord.split("")
 
     // display word into underscores
     for (var i = 0; i < chosenWord.length; i++) {
-        console.log(i);
         underScores.push("_");
     }
-    console.log(underScores)
 
     document.getElementById("guessWord").innerHTML = underScores.join(" ");
-    console.log(randomWords.indexOf(chosenWord));
 
-    document.onkeyup = function () {
+
+
+}
+document.onkeyup = function () {
+    if (event.key.toLowerCase() !== "meta") {
+
         var userGuess = event.key.toLowerCase();
-        console.log("User Guess: " + userGuess);
-        document.getElementById("guessesLeft").innerHTML = totalGuesses;
+        var correctGuess = false;
+        for (var i = 0; i < lettersGuessed.length; i++) {
+            var element = lettersGuessed[i];
+            if (element === userGuess) {
+                return;
+            }
 
+        }
         for (var i = 0; i < chosenWord.length; i++) {
             if (chosenWord[i] === userGuess) {
                 underScores[i] = userGuess;
+                correctGuess = true;
             } else {
                 lettersGuessed.push(userGuess);
-                totalGuesses--;
             }
         }
         document.getElementById("guessWord").innerHTML = underScores.join(" ");
-
         lettersGuessedText = lettersGuessedText + userGuess;
 
         document.getElementById("lettersGuessed").innerHTML = lettersGuessedText + " ";
 
-        console.log(chosenWord, underScores, lettersGuessedText)
-    }
+        if (!correctGuess) {
+            wrongGuesses--;
+            document.getElementById("guessesLeft").innerHTML = wrongGuesses;
+            console.log(wrongGuesses);
+        } else {
+            if (chosenWord === underScores.join('')) {
+                wins++;
+                document.getElementById("totalWins").innerHTML = wins;
+                confirmations('win');
+            }
+        }
 
+        if (wrongGuesses < 1) {
+            confirmations('lose');
+        }
+
+    }
 }
-// Fix wrong guesses to only take away 1 guesses left
-// when a person guesses the entire word, the game starts over and total wins tracker +1
-// if guesses left gets to 0, restart game and prompt "try again"
+
+function confirmations(winOrLose) {
+    if (winOrLose === 'lose') {
+        setTimeout(() => {
+            var restartGame = confirm("Would you like to try again?")
+            if (restartGame) {
+                startGame();
+            }
+        }, 100);
+    } else {
+        setTimeout(() => {
+            var winConfirm = confirm("Congratulations, you're a wizard! Would you like to play again?");
+            if (winConfirm) {
+                startGame();
+            }
+        }, 100);
+    }
+}
 
 
 startGame();
